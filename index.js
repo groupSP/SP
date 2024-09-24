230f753fb8c3b2a55a408e0642ad10e4cb5edfa1
xys012 <a1867906@adelaide.edu.au>
2024-09-24 23:15:39 +0930

    initial

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const path = require('path')
-const http = require('http')
+const express = require('express');
+const path = require('path');
+const http = require('http');
 
-const app = express()
-const server = http.createServer(app)
+const app = express();
+const server = http.createServer(app);
 
-console.log('test')
+port = 9000;
+
+app.use(express.static(__dirname));
+
+server.listen(port, () => {
+    console.log(`port is ${port}!`);
+})
